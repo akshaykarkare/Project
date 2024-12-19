29c0b1994dc72ea2adf3f715595916dc716f6627
karkareakshay <akarkare@tibco.com>
2024-12-19 15:35:28 +0530

    added testNG runner

--- src/test/java/TestNGRunner.java
+++ src/test/java/TestNGRunner.java
@@ -1,2 +1,12 @@
-public class TestNGRunner {
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = "src/test/features",
+        glue = {"stepdefinitions"},
+        plugin = {"html:target/cucumber-reports/htmlReports.html","json:target/cucumber-reports/cucumber.json","rerun:target/rerun.txt"},
+        tags = "@demo",
+        monochrome = true
+)
+public class TestNGRunner extends AbstractTestNGCucumberTests {
 }
